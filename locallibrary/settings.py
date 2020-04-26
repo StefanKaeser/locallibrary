3679e3e5a9af84d964a6e7a10561954eca9fe7df
Stefan <stefan.kaeser7@gmail.com>
2020-04-26 17:33:25 +0200

    try to debug following stack
    https://stackoverflow.com/questions/52977215/heroku-bad-request-server-error-500-for-django-web-app

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -145,3 +145,20 @@ STATIC_URL = '/static/'
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
